9f60ed900a472123318cbff7329454f303be2572
n.vasilishin <n.vasilishin@argustelecom.ru>
2015-09-28 20:01:11 +0300

    andr

---
+++ android/MaterialDrawerFragmentSample/app/src/main/java/com/sample/drawer/fragments/FragmentSchedule.java
@@ -0,0 +1,21 @@
+package com.sample.drawer.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sample.drawer.R;
+
+
+public class FragmentSchedule extends Fragment {
+    public final static String NAVI_DRAW_ITEM_NAME = "Schedule";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+
+}
---
+++ android/MaterialDrawerFragmentSample/app/src/main/java/com/sample/drawer/fragments/FragmentStatistics.java
@@ -0,0 +1,20 @@
+package com.sample.drawer.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sample.drawer.R;
+
+
+public class FragmentStatistics extends Fragment {
+    public final static String NAVI_DRAW_ITEM_NAME = "Statistics";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
+    }
+}
