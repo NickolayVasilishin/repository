88dba46bc67fb08480a77642402d1eabbe378166
n.vasilishin <n.vasilishin@argustelecom.ru>
2015-10-24 18:42:00 +0300

    Xml packet extractor

---
+++ java/workspace-hadoop/hadoop-mapreduce-xml/src/main/java/com/nv/hadoop/PacketExtractor.java
@@ -0,0 +1,42 @@
+package com.nv.hadoop;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
+public class PacketExtractor {
+	public static final String START_TAG = "<packet>";
+	public static final String END_TAG = "</packet>";
+	public static final String queryToTcp = "//proto[@name= 'tcp']";
+	public static final String queryToUdp = "//proto[@name= 'udp']";
+	public static final String queryToIpSource = "/packet/proto[@name= 'ip']/field[@name='ip.src']";
+	
+	private Element root;
+
+	public PacketExtractor(String xmlPacket) throws JDOMException, IOException {
+		SAXBuilder builder = new SAXBuilder();
+		Reader in = new StringReader(xmlPacket);
+		Document doc = builder.build(in);
+		Element root = doc.getRootElement();
+		if(!root.getName().equals(START_TAG))
+			throw new IllegalArgumentException("Passed xml element is not a <packet>");
+	}
+	
+	
+	
+	private String getIpSourceAddress(){
+		return XPathFactory.instance().compile(queryToIpSource, Filters.element()).evaluate(root).get(0).getAttributeValue("show");
+	}
+	
+	public boolean isTcpPacket(){
+		return !XPathFactory.instance().compile("//proto[@name= 'tcp']", Filters.element()).evaluate(root).isEmpty();
+	}
+}
---
+++ java/workspace-hadoop/hadoop-mapreduce-xml/src/main/java/com/nv/hadoop/TcpPacket.java
@@ -0,0 +1,5 @@
+package com.nv.hadoop;
+
+public class TcpPacket {
+	
+}
