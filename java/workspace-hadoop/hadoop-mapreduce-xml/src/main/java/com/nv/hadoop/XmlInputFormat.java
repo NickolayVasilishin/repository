76c9eb4207ade3d22cb9ea83ecdf1fd574d0d890
n.vasilishin <n.vasilishin@argustelecom.ru>
2015-10-29 19:08:35 +0300

    hd works, mr does nothing

--- java/workspace-hadoop/hadoop-mapreduce-xml/src/main/java/com/nv/hadoop/XmlInputFormat.java
+++ java/workspace-hadoop/hadoop-mapreduce-xml/src/main/java/com/nv/hadoop/XmlInputFormat.java
@@ -1,15 +1,16 @@
 package com.nv.hadoop;
 
 import java.io.IOException;
+
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DataOutputBuffer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
